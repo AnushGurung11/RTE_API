a55eb686e80ec95ab2174eedc113410b3d08b441
Sneha Giri <sneha.giri@email.com>
2025-07-22 18:02:24 +0545

    Add TeacherViewSet and UploadTeacherExcelView with router and upload API

--- rte_api/teachers/urls.py
+++ rte_api/teachers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeacherViewSet, UploadTeacherExcelView
+from teachers.views import TeacherViewSet, UploadTeacherExcelView
 
 router = DefaultRouter()
 router.register(r'teachers', TeacherViewSet)
