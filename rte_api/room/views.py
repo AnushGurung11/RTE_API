5935eb6d5d36a2c3335a6839236c07eb85b84ea6
AnushGurung11 <anushgrg15@gmai.com>
2025-07-24 17:32:32 +0545

    Added View sets for Room and seat

--- rte_api/room/views.py
+++ rte_api/room/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Room, Seat
+from .serializers import RoomSerializer, SeatSerializer
 
-# Create your views here.
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
